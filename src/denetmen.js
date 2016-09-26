7c63fbdd0ce5f9edbfac4105b9bf3b2939f447d5
İzni Burak Demirtaş <izniburak@outlook.com>
2016-09-26 17:18:34 +0300

    consolo.log commands deleted.

--- src/denetmen.js
+++ src/denetmen.js
@@ -32,16 +32,15 @@
             $($this).find('[data-' + o.item + ']').each(function(index, elem)
             {
                 var $el = $(this);
-                console.log($el);
                 if( $.trim($el.val()) == '' || (($el.attr('type') == 'checkbox' || $el.attr('type') == 'radio') && $el.is(':checked') === false) )
                 {
                     message = ( $.trim($el.attr('data-' + o.item)) === '' ? o.default + ' ('+ $el.attr('name').toUpperCase() +')' : $el.attr('data-' + o.item));
                     
                     if(o.modal)
                     {
-                        var title        = ( typeof(o.modal.title) === 'undefined' ? 'Oppss!' : o.modal.title);
-                        var button        = ( typeof(o.modal.button) === 'undefined' ? 'OK' : o.modal.button);
-                        var buttonClass    = ( typeof(o.modal.buttonClass) === 'undefined' ? 'primary' : o.modal.buttonClass);
+                        var title       = ( typeof(o.modal.title) === 'undefined' ? 'Oppss!' : o.modal.title);
+                        var button      = ( typeof(o.modal.button) === 'undefined' ? 'OK' : o.modal.button);
+                        var buttonClass = ( typeof(o.modal.buttonClass) === 'undefined' ? 'primary' : o.modal.buttonClass);
                         var fade        = ( typeof(o.modal.fade) === 'undefined' ? false : o.modal.fade);
                         
                         var modalLayout = '<div id="denetmenjs-modal" class="modal'+(fade === true ? ' fade' : '')+'" tabindex="-1" role="dialog"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">'+title+'</h4></div><div class="modal-body">'+message+'</div><div class="modal-footer"><button type="button" class="btn btn-'+buttonClass+'" data-dismiss="modal">'+button+'</button></div></div></div></div>';
